5b09a1f3091c6513e87d9faac8688cbe1d5279ee
retr0 <395637649@qq.com>
2019-07-11 19:12:12 +0800

    feat:practice6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    public int getKlass() {
+        return klass;
+    }
+
+    private int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        klass = -1;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() +
+                ((klass == -1) ? " I am a Teacher. I teach No Class." :
+                String.format(" I am a Teacher. I teach Class %d.", klass));
+    }
 }
